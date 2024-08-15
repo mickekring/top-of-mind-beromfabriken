9a1e0fb2315a907c26de6d7d0cbeea67944a76eb
Micke Kring <jag@mickekring.se>
2024-08-15 20:07:05 +0200

    0.1.4

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 
 ### Berömdrömmen
-app_version = "0.1.3"
+app_version = "0.1.4"
 ### Author: Micke Kring
 ### Contact: mikael.kring@ri.se
 
 
 import os
+import hmac
 from os import environ
 import streamlit as st
 from datetime import datetime
@@ -29,6 +30,34 @@ from mix_audio import mix_music_and_voice
 # Creates folder if they don't exist
 os.makedirs("audio", exist_ok=True) # Where audio/video files are stored for transcription
 os.makedirs("text", exist_ok=True) # Where transcribed document are beeing stored
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 
 # Check and set default values if not set in session_state
